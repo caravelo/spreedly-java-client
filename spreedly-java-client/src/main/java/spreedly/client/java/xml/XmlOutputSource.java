8d7aa8aacc890bea136bb9ee0f35c931c00561b3
Alejandro Garcia Seco <ags@changeyourflight.com>
2015-02-28 19:08:58 +0100

    Improved exception handling in XML out putter.

--- spreedly-java-client/src/main/java/spreedly/client/java/xml/XmlOutputSource.java
+++ spreedly-java-client/src/main/java/spreedly/client/java/xml/XmlOutputSource.java
@@ -3,11 +3,23 @@ package spreedly.client.java.xml;
 import java.io.IOException;
 import java.io.OutputStream;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import spreedly.client.java.exception.XmlParserException;
 import spreedly.client.java.http.OutputSource;
 
+/**
+ * Implementation of {@link spreedly.client.java.http.OutputSource} to output
+ * XMLs into HTTP body requests.
+ * 
+ * @author Alejandro Garcia Seco
+ *
+ */
 public class XmlOutputSource implements OutputSource
 {
+
+    private static final Logger log = LoggerFactory.getLogger(XmlOutputSource.class);
 
     private final XmlParser parser;
     private final Object source;
@@ -27,8 +39,9 @@ public class XmlOutputSource implements OutputSource
         }
         catch (XmlParserException e)
         {
-            // XXX: improve exception handling
-            e.printStackTrace();
+            String message = "Unparseable XML output source";
+            log.error(message);
+            throw new IOException(message, e);
         }
     }
 
